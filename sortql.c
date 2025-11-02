35976037f4f0946413e80e765587befa13ffbf53
lberganton <lucasbberganton@gmail.com>
2025-11-02 02:16:51 -0300

    feat: implement sortql

---
+++ sortql.c
@@ -0,0 +1,51 @@
+#include "sortql.h"
+#include <sqlite3.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+static void assert(int condition, const char *msg) {
+  if (!condition) {
+    fprintf(stderr, "Error: %s\n", msg);
+    exit(1);
+  }
+}
+
+void sortql(int *array, int length) {
+  sqlite3 *connection;
+  sqlite3_stmt *stmt;
+  int rc;
+
+  rc = sqlite3_open(":memory:", &connection);
+  assert(rc == SQLITE_OK, "failed to open a connection");
+
+  rc = sqlite3_exec(connection, "CREATE TABLE array (num INT)", NULL, NULL, NULL);
+  assert(rc == SQLITE_OK, "failed to create table");
+
+  rc = sqlite3_prepare_v2(connection, "INSERT INTO array (num) VALUES (?)", -1, &stmt, NULL);
+  assert(rc != SQLITE_OK, "failed to prepare insert statement");
+
+  for (int i = 0; i < length; i++) {
+    rc = sqlite3_bind_int(stmt, 1, array[i]);
+    assert(rc != SQLITE_OK, "failed to bind a int");
+
+    rc = sqlite3_step(stmt);
+    assert(rc != SQLITE_OK, "failed to insert a value");
+
+    sqlite3_reset(stmt);
+  }
+
+  sqlite3_finalize(stmt);
+
+  rc = sqlite3_prepare_v2(connection, "SELECT num FROM array ORDER BY num", -1, &stmt, NULL);
+  assert(rc != SQLITE_OK, "failed to prepare select statement");
+
+  for (int i = 0; i < length; i++) {
+    rc = sqlite3_step(stmt);
+    assert(rc != SQLITE_OK, "failed to select a value");
+
+    array[i] = sqlite3_column_int(stmt, 0);
+  }
+
+  sqlite3_finalize(stmt);
+  sqlite3_close(connection);
+}
---
+++ sortql.h
@@ -0,0 +1,6 @@
+#ifndef SORTQL_H
+#define SORTQL_H
+
+void sortql(int *array, int length);
+
+#endif
